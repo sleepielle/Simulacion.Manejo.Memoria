abe9fa83617f8f8ccb4788f10bf68ad269d155a5
sleepielle <pggisselle@gmail.com>
2023-06-18 19:50:41 -0600

    parte 1 cambios

--- prueba.cpp
+++ prueba.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <iomanip>
 #include <vector>
+#include <bitset>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
@@ -38,46 +42,121 @@ void loadMemoryTrace(const string &memoryFile, vector<MemoryReference> &memoryTr
     }
 }
 
-void generateMemoryMap(const vector<MemoryReference> &memoryTrace, unordered_map<string, MemoryMapping> &memoryMap)
+void generatePhysicalMemoryMap(const vector<MemoryReference> &physicalMemoryTrace, unordered_map<string, MemoryMapping> &physicalMemoryMap)
 {
     const int pageSize = 4096;
-    const int frameSize = 4096;
-    int nextPage = 0;
+    const int numFrames = 256;
     int nextFrame = 0;
 
-    for (const MemoryReference &reference : memoryTrace)
+    for (const MemoryReference &reference : physicalMemoryTrace)
     {
         string address = reference.address;
         string page = address.substr(0, address.length() - 3); // Obtener la página (primeros dígitos de la dirección)
 
-        if (memoryMap.find(page) == memoryMap.end())
+        if (physicalMemoryMap.find(page) == physicalMemoryMap.end())
         {
             // La página aún no ha sido asignada a un marco
             MemoryMapping mapping;
-            mapping.page = page;
+            mapping.page = ""; // No se utiliza en el mapa de memoria física
             mapping.frame = to_string(nextFrame);
             mapping.dirty = (reference.operation == 'W');
-            memoryMap[page] = mapping;
+            physicalMemoryMap[page] = mapping;
 
-            nextFrame++;
+            nextFrame = (nextFrame + 1) % numFrames;
         }
         else
         {
             // La página ya ha sido asignada a un marco
-            MemoryMapping &mapping = memoryMap[page];
+            MemoryMapping &mapping = physicalMemoryMap[page];
             mapping.dirty = mapping.dirty || (reference.operation == 'W');
         }
     }
 }
-
-void printMemoryMap(const unordered_map<string, MemoryMapping> &memoryMap)
+void generateVirtualMemoryMap(const vector<MemoryReference> &virtualMemoryTrace, unordered_map<string, MemoryMapping> &virtualMemoryMap)
 {
-    cout << setw(15) << left << "Dirección" << setw(10) << "Operación  " << setw(10) << "Página" << setw(10) << "Marco" << setw(10) << "Bit de Modificación" << endl;
+    const int pageSize = 4096;
+    const int numPages = 1048576;
+    const int numFrames = 256;
+    int nextPage = 0;
+    int nextFrame = 0;
 
-    for (const auto &entry : memoryMap)
+    for (const MemoryReference &reference : virtualMemoryTrace)
     {
-        const MemoryMapping &mapping = entry.second;
-        cout << setw(15) << left << mapping.page + "XXX" << setw(10) << (mapping.dirty ? "W" : "R") << setw(10) << mapping.page << setw(10) << mapping.frame << setw(10) << (mapping.dirty ? "\033[1;31m1\033[0m" : "0") << endl;
+        string address = reference.address;
+        string page = address.substr(0, address.length() - 3); // Obtener la página (primeros dígitos de la dirección)
+        int pageNumber = stoi(page);                           // Convertir el número de página a entero
+
+        if (pageNumber < numPages)
+        {
+            // La página no excede el número máximo de páginas
+            if (nextFrame >= numFrames)
+            {
+                // No hay más marcos de página disponibles
+                MemoryMapping mapping;
+                mapping.page = page;
+                mapping.frame = "Sin asignar";
+                mapping.dirty = (reference.operation == 'W');
+                virtualMemoryMap[page] = mapping;
+            }
+            else if (virtualMemoryMap.find(page) == virtualMemoryMap.end())
+            {
+                // La página aún no ha sido asignada a un marco
+                MemoryMapping mapping;
+                mapping.page = page;
+                mapping.frame = to_string(nextFrame);
+                mapping.dirty = (reference.operation == 'W');
+                virtualMemoryMap[page] = mapping;
+
+                nextFrame++;
+            }
+            else
+            {
+                // La página ya ha sido asignada a un marco
+                MemoryMapping &mapping = virtualMemoryMap[page];
+                mapping.dirty = mapping.dirty || (reference.operation == 'W');
+            }
+        }
+        else
+        {
+            // El número de página excede el número máximo de páginas
+            int adjustedPageNumber = pageNumber % numPages; // Ajustar el número de página dentro del rango permitido
+            MemoryMapping mapping;
+            mapping.page = to_string(adjustedPageNumber); // Representar el número de página ajustado con dígitos arábigos
+            mapping.frame = nextFrame < numFrames ? to_string(nextFrame) : "Sin asignar";
+            mapping.dirty = (reference.operation == 'W');
+            virtualMemoryMap[page] = mapping;
+
+            nextFrame++;
+        }
+    }
+}
+
+void printVirtualMemoryMap(const unordered_map<string, MemoryMapping> &virtualMemoryMap, const vector<MemoryReference> &virtualMemoryTrace)
+{
+    cout << setw(15) << left << "Dirección" << setw(10) << "Operación" << setw(15) << "Página" << setw(10) << "Marco" << setw(10) << "Bit de Modificación" << endl;
+
+    for (const MemoryReference &reference : virtualMemoryTrace)
+    {
+        const string &address = reference.address;
+        const string page = address.substr(0, address.length() - 3); // Obtener la página (primeros dígitos de la dirección)
+
+        const MemoryMapping &mapping = virtualMemoryMap.at(page);
+        cout << setw(15) << left << address << setw(10) << reference.operation << setw(15) << mapping.page << setw(10) << mapping.frame << setw(10) << (mapping.dirty ? "\033[1;31m1\033[0m" : "0") << endl;
+    }
+}
+
+void printPhysicalMemoryMap(const unordered_map<string, MemoryMapping> &physicalMemoryMap, const vector<MemoryReference> &physicalMemoryTrace)
+{
+    cout << setw(15) << left << "Dirección" << setw(10) << "Operación" << setw(10) << "Marco" << setw(10) << "Bit de Modificación" << endl;
+
+    for (size_t i = 0; i < physicalMemoryTrace.size(); i++)
+    {
+        const MemoryReference &reference = physicalMemoryTrace[i];
+        const string &address = reference.address;
+        const string page = address.substr(0, address.length() - 3); // Obtener la página (primeros dígitos de la dirección)
+
+        const MemoryMapping &mapping = physicalMemoryMap.at(page);
+        cout << setw(15) << left << address << setw(10) << reference.operation << setw(10) << mapping.frame << setw(10) << (mapping.dirty ? "\033[1;31m1\033[0m" : "0") << endl;
     }
 }
 
@@ -99,59 +178,20 @@ int main(int argc, char *argv[])
     loadMemoryTrace(virtualMemoryFile, virtualMemoryTrace, numAddresses);
     loadMemoryTrace(physicalMemoryFile, physicalMemoryTrace, numAddresses);
 
-    generateMemoryMap(virtualMemoryTrace, virtualMemoryMap);
-    generateMemoryMap(physicalMemoryTrace, physicalMemoryMap);
+    generateVirtualMemoryMap(virtualMemoryTrace, virtualMemoryMap);
+    generatePhysicalMemoryMap(physicalMemoryTrace, physicalMemoryMap);
 
     cout << setw(15) << "\n\033[1;33mMapa de memoria\033[0m\n"
          << endl;
     cout << setw(15) << left << "\033[1;32mMapa Virtual\033[0m" << std::endl;
-    printMemoryMap(virtualMemoryMap);
+    printVirtualMemoryMap(virtualMemoryMap, virtualMemoryTrace);
 
     cout << endl;
 
-    cout << setw(15) << left << "\033[1;32mMapa Fisico\033[0m" << std::endl;
-    printMemoryMap(physicalMemoryMap);
-    cout << endl
-         << endl;
+    /*     cout << setw(15) << left << "\033[1;32mMapa Fisico\033[0m" << std::endl;
+        printPhysicalMemoryMap(physicalMemoryMap, physicalMemoryTrace);
+        cout << endl
+             << endl; */
 
     return 0;
 }
-
-/* #include <iostream>
-#include <istream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
-bool readPhysicalMemory(string);
-bool writeVirtualMemory(string);
-
-int main(int argc, char *argv[])
-{
-
-    string memoryAddress = argv[1];
-    string operation = argv[2];
-
-    string virtualMemoryFile = "bzip.trace";
-    string physicalMemoryFile = "gcc.trace";
-
-    readPhysicalMemory(physicalMemoryFile);
-}
-
-bool writeVirtualMemory(string memoryFile)
-{
-    return true;
-}
-
-bool readPhysicalMemory(string memoryFile)
-{
-    std::ifstream file(memoryFile);
-    std::string line;
-
-    while (getline(file, line))
-    {
-        cout << line << endl;
-    }
-    return true;
-} */
